9f70b655c0d839f5e9daf39954c14e802a1c1e7c
Ahmad-Hazrati <>
2023-09-11 09:43:28 +0000

    Add basic html temlate

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -22,13 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-ahmad-hazrati-ci-fsf-hel-oydp6a7mw8.us2.codeanyapp.com']
+ALLOWED_HOSTS = ['8000-ahmad-hazrati-ci-fsf-hel-8htfnc11mm.us2.codeanyapp.com']
 
 
 # Application definition
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todo',
 ]
 
 MIDDLEWARE = [
